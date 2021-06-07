7853cc1696d9fab0d7cd264336a6e9ec65f4cdc4
Sandro Paganotti <sandro.paganotti@gmail.com>
2021-06-07 23:46:52 +0100

    added loading screen

--- js-src/index.js
+++ js-src/index.js
@@ -6,21 +6,21 @@ import * as CocoSsd from "@tensorflow-models/coco-ssd";
 const pace = window.requestIdleCallback || window.requestAnimationFrame;
 
 function getStreamUrl() {
-  const default_stream = "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8";
+  const default_stream = "https://test-streams.mux.dev/test_001/stream.m3u8";
   const params = new URLSearchParams(document.location.search.substring(1));
   return params.get("m3u8") || default_stream;
 }
 
-function createVideoStream(url) {
-  const video = document.createElement("video");
+function getClasses() {
+  const default_classes = "person";
+  const params = new URLSearchParams(document.location.search.substring(1));
+  return (params.get("classes") || default_classes).split(",");
+}
+
+function createVideoStream(video, url) {
   video.setAttribute("crossOrigin", "anonymous");
   if (Hls.isSupported()) {
-    var hls = new Hls({
-      debug: true,
-      enableWorker: true,
-      lowLatencyMode: true,
-      backBufferLength: 90,
-    });
+    var hls = new Hls();
     hls.loadSource(url);
     hls.attachMedia(video);
     hls.on(Hls.Events.MEDIA_ATTACHED, () => video.play());
@@ -29,15 +29,22 @@ function createVideoStream(url) {
     video.addEventListener("canplay", () => video.play());
   }
   return new Promise((resolve) =>
-    video.addEventListener("loadeddata", () => resolve(video))
+    video.addEventListener("loadeddata", () => {
+      video.setAttribute("hidden", "hidden");
+      resolve(video);
+    })
   );
 }
 
-async function startDetection(video) {
-  const model = await CocoSsd.load();
+async function startDetection(model, video) {
   let detectionCallback = null;
   const callback = async () => {
-    const predictions = await model.detect(video);
+    let predictions = [];
+    try {
+      predictions = await model.detect(video);
+    } catch (error) {
+      pace(callback);
+    }
     if (detectionCallback) {
       detectionCallback(predictions);
     }
@@ -52,25 +59,38 @@ async function startDetection(video) {
 function drawPredictions(video, onDetection) {
   const canvas = document.createElement("canvas");
   const context = canvas.getContext("2d");
+  const categories = getClasses();
   canvas.width = video.videoWidth;
   canvas.height = video.videoHeight;
   onDetection((predictions) => {
     context.drawImage(video, 0, 0);
-    console.log(predictions);
-    predictions.forEach(({ bbox: [x, y, w, h], category, score }) => {
-      context.beginPath();
-      context.rect(x, y, w, h);
-      context.stroke();
+    predictions.forEach(({ bbox: [x, y, w, h], class: category, score }) => {
+      if (score > 0.5 && categories.includes(category)) {
+        context.beginPath();
+        context.rect(x, y, w, h);
+        context.stroke();
+      }
     });
   });
   return canvas;
 }
 
+function setLoadingContent(loading, text) {
+  loading.textContent = text;
+}
+
 async function init() {
-  const video = await createVideoStream(getStreamUrl());
-  const onDetection = await startDetection(video);
+  const loadingNode = document.getElementById("loading");
+  const videoNode = document.getElementById("video");
+  setLoadingContent(loadingNode, "Loading Model...");
+  const model = await CocoSsd.load();
+  setLoadingContent(loadingNode, "Creating Video Stream...");
+  const video = await createVideoStream(videoNode, getStreamUrl());
+  setLoadingContent(loadingNode, "Starting Detection...");
+  const onDetection = await startDetection(model, video);
+  loadingNode.classList.add("hidden");
   const canvas = drawPredictions(video, onDetection);
-  document.body.appendChild(canvas);
+  videoNode.parentNode.appendChild(canvas);
 }
 
 init();
